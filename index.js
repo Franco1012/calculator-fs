a6a07db2d65769a819f75debd0a7dc5c08aa74f5
Franco1012 <solerfrancogerman@gmail.com>
2024-07-06 12:09:39 -0300

    se agregan funciones

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import multiplicacion from "./operaciones/multiplicacion.js";
-const module={multiplicacion}//exportar el objeto me facilita la desestructuracion
-export {multiplicacion}
+import suma from "./operaciones/suma.js";
+import resta from "./operaciones/resta.js";
+import division from "./operaciones/division.js";
+const module = { multiplicacion, suma, resta, division }//exportar el objeto me facilita la desestructuracion
+export { multiplicacion, suma, resta, division }
 export default module
