e212b8b15fc9f98b9e6e346d9be2a2a1bfd39902
Franco1012 <solerfrancogerman@gmail.com>
2024-07-06 10:43:02 -0300

    multiplicacion

---
+++ index.js
@@ -0,0 +1,4 @@
+import multiplicacion from "./operaciones/multiplicacion.js";
+
+export const module={multiplicacion}//exportar el objeto me facilita la desestructuracion
+export default module
---
+++ operaciones/suma.js
@@ -0,0 +1,4 @@
+const suma = (n1, n2) => {
+    return n1 + n2;
+}
+export default suma
